0c7fa3410822fe2626efc54d67caef1f38d131d9
Magnus Mengelbier <49205696+mmengelbier@users.noreply.github.com>
2024-10-27 00:20:00 +0200

    Update sas-createrelease.sh

--- .scripts/sas-createrelease.sh
+++ .scripts/sas-createrelease.sh
@@ -45,7 +45,9 @@ RELEASE_CREATE=$( curl -sS -L -X POST -H "Accept: application/vnd.github+json" -
                       -d "{\"tag_name\":\"${GITHUB_REF_NAME}\", \"name\":\"${GITHUB_REF_NAME}\", \"body\":\"Description of the release\",\"draft\":true, \"prerelease\":false, \"generate_release_notes\":false}" )
 
 
-RELEASE_ID=$( echo ${RELEASE_CREATE} | jq .id )
+RELEASE_ID=$( echo ${RELEASE_CREATE} | jq -r .id )
+
+echo "${RELEASE_ID}"
 
 # -- add assets
 
