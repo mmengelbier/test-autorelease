9f0f9a821a842177a1dee3b12374ba65c6497754
Magnus Mengelbier <49205696+mmengelbier@users.noreply.github.com>
2024-10-26 23:03:50 +0200

    d

--- .scripts/sas-createrelease.sh
+++ .scripts/sas-createrelease.sh
@@ -17,19 +17,44 @@ echo "Reference ${GITHUB_REF}"
 
 
 # -- generate release label 
-RELEASE_STRING="${VARS_COMPONENT}_$( echo ${GITHUB_REF_NAME} | tr -d v )-${GITHUB_SHA}"
+RELEASE_SHORT_LABEL=${VARS_COMPONENT}_$( echo ${GITHUB_REF_NAME} | tr -d v ).zip
+RELEASE_LONG_LABEL=${RELEASE_SHORT_LABEL}-${GITHUB_SHA}.zip
 
-echo "${RELEASE_STRING}"
 
-
-# -- identify folders to include
+# -- create archive
 cd /src
 
-zip -r ../${RELEASE_STRING}.zip $(ls)
+echo "-- create archive ${RELEASE_LONG_LABEL}"
+zip -r ../${RELEASE_LONG_LABEL} -x '.keep' $@
 
-cp ../${RELEASE_STRING}.zip ../${VARS_COMPONENT}_$( echo ${GITHUB_REF_NAME} | tr -d v ).zip
+echo "-- create archive ${RELEASE_SHORT_LABEL}"
+cp ../${RELEASE_LONG_LABEL} ../${RELEASE_SHORT_LABEL}
 
 ls -alF /
+
+
+# -- create release as draft
+
+DRAFT_RELEAE=$(curl -L \
+                    -X POST \
+                    -H "Accept: application/vnd.github+json" \
+                    -H "Authorization: Bearer ${ACTION_TOKEN}" \
+                    -H "X-GitHub-Api-Version: 2022-11-28" \
+                    ${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases \
+                      -d '{"tag_name":"${GITHUB_REF}","target_commitish":"${GITHUB_SHA}","name":"${GITHUB_REF}","body":"Description of the release","draft":true,"prerelease":false,"generate_release_notes":false}')
+
+
+echo "${DRAFT_RELEASE}"
+
+
+
+# -- add assets
+
+
+# -- set release as final
+
+
+
 
 
 
