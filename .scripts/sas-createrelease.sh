0d8921432de9c2cd679893502003e6e230265aab
Magnus Mengelbier <49205696+mmengelbier@users.noreply.github.com>
2024-10-26 20:32:36 +0200

    b

---
+++ .scripts/sas-createrelease.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#
+# Create a SAS macro release
+#
+#
+#
+
+
+
+# -- identify commiit
+echo "Processing commit ${GITHUB_SHA}"
+
+
+# -- identify reference
+echo "Reference ${GITHUB_REF}"
